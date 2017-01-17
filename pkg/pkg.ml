aaa3d4777a24871704d7dd8218f1480dcd0b4c77
Hannes Mehnert <hannes@mehnert.org>
2017-01-17 18:46:08 +0000

    release preparation, make lwt_tls and mirage_tls explicit

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -5,20 +5,21 @@ open Topkg
 
 let lwt = Conf.with_pkg ~default:false "lwt"
 let mirage = Conf.with_pkg ~default:false "mirage"
-let tls = Conf.with_pkg ~default:false "tls"
+let lwt_tls = Conf.with_pkg ~default:false "lwt-tls"
+let mirage_tls = Conf.with_pkg ~default:false "mirage-tls"
 
 let () =
-  let opams = [ Pkg.opam_file "opam" ~lint_deps_excluding:(Some ["io-page"]) ] in
-  Pkg.describe ~opams "logs-syslog" @@ fun c ->
+  Pkg.describe "logs-syslog" @@ fun c ->
   let lwt = Conf.value c lwt
   and mirage = Conf.value c mirage
-  and tls = Conf.value c tls
+  and lwt_tls = Conf.value c lwt_tls
+  and mirage_tls = Conf.value c mirage_tls
   in
   Ok [
     Pkg.mllib "src/logs-syslog.mllib" ;
     Pkg.mllib "src/logs-syslog-unix.mllib" ;
     Pkg.mllib ~api:["Logs_syslog_lwt"] ~cond:lwt "src/logs-syslog-lwt.mllib" ;
-    Pkg.mllib ~api:["Logs_syslog_lwt_tls"] ~cond:(lwt && tls) "src/logs-syslog-lwt-tls.mllib" ;
+    Pkg.mllib ~api:["Logs_syslog_lwt_tls"] ~cond:lwt_tls "src/logs-syslog-lwt-tls.mllib" ;
     Pkg.mllib ~api:["Logs_syslog_mirage"] ~cond:mirage "src/logs-syslog-mirage.mllib" ;
-    Pkg.mllib ~api:["Logs_syslog_mirage_tls"] ~cond:(mirage && tls) "src/logs-syslog-mirage-tls.mllib" ;
+    Pkg.mllib ~api:["Logs_syslog_mirage_tls"] ~cond:mirage_tls "src/logs-syslog-mirage-tls.mllib" ;
   ]
